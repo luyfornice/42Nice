103dc20db436efb097708aebd3daa1a925d3e51d
Luy Nagda <lnagda@c2r6p8.42nice.fr>
2023-07-13 17:55:32 +0200

    Fixing c02/ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: lnagda <lnagda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 19:04:07 by lnagda            #+#    #+#             */
-/*   Updated: 2023/07/13 15:44:11 by lnagda           ###   ########.fr       */
+/*   Updated: 2023/07/13 17:53:26 by lnagda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
